41af0ad5748a6d09245ddd99919e04f80045981b
Luna <jeppek04@gmail.com>
2023-01-15 02:53:42 +0100

    Add delay
    Added a small delay to let Pterodactyl do first run configs.

--- start.sh
+++ start.sh
@@ -1,7 +1,13 @@
+if [ !-f hasrun ]; then
+echo -e "Running first run configs. Please wait 15 seconds."
+sleep 15
 if [ -f general.yml ]; then
 mv general.yml autoplug/general.yml
 fi
 if [ -f updater.yml ]; then
 mv updater.yml autoplug/updater.yml
 fi
+echo 'The server has run more than once.' > hasrun
+else
 java -Xms128M -Xmx"$1"M -Dterminal.jline=false -Dterminal.ansi=true -jar "$2"
+fi
