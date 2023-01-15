8f70e80881a487463717511c0caa74679d293a12
Luna <jeppek04@gmail.com>
2023-01-15 18:36:33 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,5 +4,5 @@ echo "Waiting for congfigs to apply. Please wait 5 seconds."
 sleep 5
 echo 'The server has run more than once.' > hasrun
 else
-java -Xms128M -Xmx"$1"M -Dterminal.jline=false -Dterminal.ansi=true -jar "$2"
+java -Xmx8m -Dterminal.jline=false -Dterminal.ansi=true -jar "$2"
 fi
