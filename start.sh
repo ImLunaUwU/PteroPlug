61f0c24edc6fccb1bff9e9579cef45e43562f867
Luna <jeppek04@gmail.com>
2023-01-15 18:58:43 +0100

    Start AutoPlug on first run
    Also start AP on first run without making Pterodactyl think it crashed.

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@ if [ ! -f hasrun ]; then
 echo "Waiting for congfigs to apply. Please wait 5 seconds."
 sleep 5
 echo 'The server has run more than once.' > hasrun
+java -Xms128M -Xmx500M -Dterminal.jline=false -Dterminal.ansi=true -jar "$1"
 else
-java -Xmx8m -Dterminal.jline=false -Dterminal.ansi=true -jar "$1"
+java -Xms128M -Xmx500M -Dterminal.jline=false -Dterminal.ansi=true -jar "$1"
 fi
